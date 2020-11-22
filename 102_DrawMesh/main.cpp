986d7c23501aeb0b3d8c7242581c570209027f37
hbansal987 <30906656+hbansal987@users.noreply.github.com>
2020-11-23 01:39:09 +0530

    Add files via upload

--- 102_DrawMesh/main.cpp
+++ 102_DrawMesh/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char * argv[])
 {
   igl::opengl::glfw::Viewer viewer;
   const auto names =
-    {"camel-01.obj","camel-02.obj","camel-03.obj","camel-04.obj","camel-05.obj"};
+    {"lion-01.obj","lion-02.obj","lion-03.obj","lion-04.obj","lion-05.obj"};
   std::map<int, Eigen::RowVector3d> colors;
   int last_selected = -1;
   for(const auto & name : names)
@@ -60,28 +60,84 @@ int main(int argc, char * argv[])
   };
 
 
+  igl::readOBJ(TUTORIAL_SHARED_PATH "/lion-01.obj", V, F); //Original
+  igl::readOBJ(TUTORIAL_SHARED_PATH "/lion-02.obj", V2, F2); //Reference
 
-  igl::readOBJ(TUTORIAL_SHARED_PATH "/camel-01.obj", V, F);
-  igl::readOBJ(TUTORIAL_SHARED_PATH "/camel-02.obj", V2, F2);
+  std::cout<<F.size()<<std::endl;
+  std::cout<<V.size()<<std::endl;    
 
-  Eigen::MatrixXd v_vector(1,3);
-  v_vector(0,0) = V(0,0);
-  v_vector(0,1) = V(0,1);
-  v_vector(0,2) = V(0,2);
+  //Formation of feature vector 
+  // f = Gx
 
-  Eigen::MatrixXd v2_vector(1,3);
-  v2_vector(0,0) = V2(0,0);
-  v2_vector(0,1) = V2(0,1);
-  v2_vector(0,2) = V2(0,2);
+  //F has the indices of the vertices used to form the triangle
 
+  int number_of_triangles = F.size()/3; 
+  int number_of_vertices = V.size(); 
+
+  //This is for the original matrix not the reference. All the vertices are put in a single matrix form (x1...xn,y1....yn,z1.....zn)
   
+  Eigen::MatrixXd x_matrix(3*number_of_vertices,1);
+  for(int i=0;i<number_of_vertices/3;i=i+3){
+      x_matrix(i,0) = V(i,0);
+      x_matrix(i+1,0) = V(i,1);
+      x_matrix(i+2,0) = V(i,2);
+  }
 
-  //std::cout<<V(0,1)<<std::endl;
-  //std::cout<<V2(0,1)<<std::endl;
+  //This is for the reference matrix.
+  Eigen::MatrixXd G_matrix_small(3*number_of_triangles,number_of_vertices);
+  for(int i=0;i<3*number_of_triangles;i++){
+    for(int j=0;j<number_of_vertices;j++){
+      G_matrix_small(i,j) = 0;   
+    }
+  }
 
-  Eigen::MatrixXd Tj = v_vector*v2_vector.inverse();
+  for(int i=0;i<number_of_triangles;i++){
+  
+  int vertex1 = F2(i,0);
+  int vertex2 = F2(i,1);
+  int vertex3 = F2(i,2);
 
-  std::cout<<Tj(0,0)<<std::endl;
+  G_matrix_small(3*i,vertex1) = V2(vertex1,0);
+  G_matrix_small(3*i+1,vertex1) = V2(vertex1,1);
+  G_matrix_small(3*i+2,vertex1) = V2(vertex1,2);
+
+  G_matrix_small(3*i,vertex2) = V2(vertex2,0);
+  G_matrix_small(3*i+1,vertex2) = V2(vertex2,1);
+  G_matrix_small(3*i+2,vertex2) = V2(vertex2,2);
+
+  G_matrix_small(3*i,vertex3) = V2(vertex3,0);
+  G_matrix_small(3*i+1,vertex3) = V2(vertex3,1);
+  G_matrix_small(3*i+2,vertex3) = V2(vertex3,2);
+
+  }
+
+
+  Eigen::MatrixXd G_matrix(9*number_of_triangles,3*number_of_vertices);
+  for(int i=0;i<9*number_of_triangles;i++){
+    for(int j=0;i<3*number_of_vertices;j++){
+      G_matrix(i,j) = 0;
+    }
+  }
+
+  for(int i=0;i<3*number_of_triangles;i++){
+    for(int j=0;i<number_of_vertices;j++){
+      G_matrix(i,j) = G_matrix_small(i,j);
+    }
+  }
+
+  for(int i=3*number_of_triangles;i<6*number_of_triangles;i++){
+    for(int j=number_of_vertices;i<2*number_of_vertices;j++){
+      G_matrix(i,j) = G_matrix_small(i-3*number_of_triangles,j-number_of_vertices);
+    }
+  }
+
+  for(int i=6*number_of_triangles;i<9*number_of_triangles;i++){
+    for(int j=2*number_of_vertices;i<3*number_of_vertices;j++){
+      G_matrix(i,j) = G_matrix_small(i-6*number_of_triangles,j-2*number_of_vertices);
+    }
+  }
+
+  Eigen::MatrixXd f = G_matrix*x_matrix;
 
   viewer.launch();
   return EXIT_SUCCESS;
