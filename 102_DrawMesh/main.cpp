91b73afdeb88eb195c2eb56496229e5cb6a6a5c5
hbansal987 <30906656+hbansal987@users.noreply.github.com>
2020-11-24 03:04:11 +0530

    Add files via upload

--- 102_DrawMesh/main.cpp
+++ 102_DrawMesh/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <map>
 #include <igl/readOBJ.h>
+#include <igl/readOFF.h>
 
 Eigen::MatrixXd V;
 Eigen::MatrixXi F;
@@ -17,68 +18,71 @@ Eigen::MatrixXd V2_inverse;
 int main(int argc, char * argv[])
 {
   igl::opengl::glfw::Viewer viewer;
-  const auto names =
-    {"lion-01.obj","lion-02.obj","lion-03.obj","lion-04.obj","lion-05.obj"};
-  std::map<int, Eigen::RowVector3d> colors;
-  int last_selected = -1;
-  for(const auto & name : names)
-  {
-    viewer.load_mesh_from_file(std::string(TUTORIAL_SHARED_PATH) + "/" + name);
-    colors.emplace(viewer.data().id, 0.5*Eigen::RowVector3d::Random().array() + 0.5);
-  }
+  // const auto names =
+  //   {"lion-01.obj","lion-02.obj","lion-03.obj","lion-04.obj","lion-05.obj"};
+  // std::map<int, Eigen::RowVector3d> colors;
+  // int last_selected = -1;
+  // for(const auto & name : names)
+  // {
+  //   viewer.load_mesh_from_file(std::string(TUTORIAL_SHARED_PATH) + "/" + name);
+  //   colors.emplace(viewer.data().id, 0.5*Eigen::RowVector3d::Random().array() + 0.5);
+  // }
 
-  viewer.callback_key_down =
-    [&](igl::opengl::glfw::Viewer &, unsigned int key, int mod)
-  {
-    if(key == GLFW_KEY_BACKSPACE)
-    {
-      int old_id = viewer.data().id;
-      if (viewer.erase_mesh(viewer.selected_data_index))
-      {
-        colors.erase(old_id);
-        last_selected = -1;
-      }
-      return true;
-    }
-    return false;
-  };
+  // viewer.callback_key_down =
+  //   [&](igl::opengl::glfw::Viewer &, unsigned int key, int mod)
+  // {
+  //   if(key == GLFW_KEY_BACKSPACE)
+  //   {
+  //     int old_id = viewer.data().id;
+  //     if (viewer.erase_mesh(viewer.selected_data_index))
+  //     {
+  //       colors.erase(old_id);
+  //       last_selected = -1;
+  //     }
+  //     return true;
+  //   }
+  //   return false;
+  // };
 
-  // Refresh selected mesh colors
-  viewer.callback_pre_draw =
-    [&](igl::opengl::glfw::Viewer &)
-  {
-    if (last_selected != viewer.selected_data_index)
-    {
-      for (auto &data : viewer.data_list)
-      {
-        data.set_colors(colors[data.id]);
-      }
-      viewer.data_list[viewer.selected_data_index].set_colors(Eigen::RowVector3d(0.9,0.1,0.1));
-      last_selected = viewer.selected_data_index;
-    }
-    return false;
-  };
+  // // Refresh selected mesh colors
+  // viewer.callback_pre_draw =
+  //   [&](igl::opengl::glfw::Viewer &)
+  // {
+  //   if (last_selected != viewer.selected_data_index)
+  //   {
+  //     for (auto &data : viewer.data_list)
+  //     {
+  //       data.set_colors(colors[data.id]);
+  //     }
+  //     viewer.data_list[viewer.selected_data_index].set_colors(Eigen::RowVector3d(0.9,0.1,0.1));
+  //     last_selected = viewer.selected_data_index;
+  //   }
+  //   return false;
+  // };
 
 
   igl::readOBJ(TUTORIAL_SHARED_PATH "/lion-01.obj", V, F); //Original
   igl::readOBJ(TUTORIAL_SHARED_PATH "/lion-02.obj", V2, F2); //Reference
 
-  std::cout<<F.size()<<std::endl;
-  std::cout<<V.size()<<std::endl;    
+  //std::cout<<F.size()<<std::endl;
+  //std::cout<<V.size()<<std::endl;    
 
-  //Formation of feature vector 
+  // Formation of feature vector 
   // f = Gx
 
-  //F has the indices of the vertices used to form the triangle
+  // F has the indices of the vertices used to form the triangle
 
   int number_of_triangles = F.size()/3; 
   int number_of_vertices = V.size(); 
+  //int number_of_vertices = 3;
 
   //This is for the original matrix not the reference. All the vertices are put in a single matrix form (x1...xn,y1....yn,z1.....zn)
   
   Eigen::MatrixXd x_matrix(3*number_of_vertices,1);
   for(int i=0;i<number_of_vertices/3;i=i+3){
+      //std::cout<<V(i,0)<<std::endl;
       x_matrix(i,0) = V(i,0);
+      //std::cout<<V(i,0)<<std::endl;
       x_matrix(i+1,0) = V(i,1);
       x_matrix(i+2,0) = V(i,2);
   }
@@ -114,25 +118,25 @@ int main(int argc, char * argv[])
 
   Eigen::MatrixXd G_matrix(9*number_of_triangles,3*number_of_vertices);
   for(int i=0;i<9*number_of_triangles;i++){
-    for(int j=0;i<3*number_of_vertices;j++){
+    for(int j=0;j<3*number_of_vertices;j++){
       G_matrix(i,j) = 0;
     }
   }
 
   for(int i=0;i<3*number_of_triangles;i++){
-    for(int j=0;i<number_of_vertices;j++){
+    for(int j=0;j<number_of_vertices;j++){
       G_matrix(i,j) = G_matrix_small(i,j);
     }
   }
 
   for(int i=3*number_of_triangles;i<6*number_of_triangles;i++){
-    for(int j=number_of_vertices;i<2*number_of_vertices;j++){
+    for(int j=number_of_vertices;j<2*number_of_vertices;j++){
       G_matrix(i,j) = G_matrix_small(i-3*number_of_triangles,j-number_of_vertices);
     }
   }
 
   for(int i=6*number_of_triangles;i<9*number_of_triangles;i++){
-    for(int j=2*number_of_vertices;i<3*number_of_vertices;j++){
+    for(int j=2*number_of_vertices;j<3*number_of_vertices;j++){
       G_matrix(i,j) = G_matrix_small(i-6*number_of_triangles,j-2*number_of_vertices);
     }
   }
